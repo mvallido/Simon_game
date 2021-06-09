8e717973d961713fc8d5a7aeccdaf11ccd2ecef4
m <vallido.michael@gmail.com>
2021-06-09 09:41:14 -0700

    Added button click animation

--- game.js
+++ game.js
@@ -14,18 +14,29 @@ function nextSequence(){
     playSound(randomChosenColour);
 }
 
-$( ".btn" ).click(function(){
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour){
+    $("."+currentColour).addClass("pressed")
+    setTimeout(function(){
+         $("."+currentColour).removeClass("pressed");
+    },100);
+}
+
+
+
+
+$(".btn").click(function(){
     var userChosenColour = this.id;
 
     userClickedPattern.push(userChosenColour);
 
     playSound(userChosenColour);
+    animatePress(userChosenColour);
 })
-
-function playSound(name){
-    var audio = new Audio("sounds/" + name + ".mp3");
-    audio.play();
-}
 
 
 
